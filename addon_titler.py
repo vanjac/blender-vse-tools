203d7581e4b0e788ffab4de14d7b9478976842fb
Jacob van't Hoog <vantjac@gmail.com>
2018-05-11 21:27:54 -0700

    Add Titler addon

---
+++ addon_titler.py
@@ -0,0 +1,44 @@
+bl_info = {
+    "name": "Titler",
+    "author": "Jacob van't Hoog",
+    "version": (1, 0),
+    "blender": (2, 75, 0),
+    "category": "Sequencer",
+    }
+
+import bpy
+import wand.image
+
+def make_title(self, context):
+    svg = ""
+    for line in context.edit_text.lines:
+        svg += line.body + "\n"
+    filename = bpy.path.abspath('//' + context.edit_text.name + '.png')
+    with wand.image.Image(blob=svg.encode('utf-8'), format='svg') as img:
+        img.save(filename=filename)
+    return filename
+
+class TEXT_MT_make_title(bpy.types.Operator):
+    """Save SVG as an image"""
+    bl_idname = "text.make_title"
+    bl_label = "Update Title"
+    bl_options = {'REGISTER'}
+
+    def execute(self, context):
+        make_title(self, context)
+        return {'FINISHED'}
+
+def make_title_menu(self, context):
+    self.layout.operator(
+        TEXT_MT_make_title.bl_idname)
+
+def register():
+    bpy.utils.register_class(TEXT_MT_make_title)
+    bpy.types.TEXT_MT_edit.append(make_title_menu)
+
+def unregister():
+    bpy.utils.unregister_class(TEXT_MT_make_title)
+    bpy.types.TEXT_MT_edit.remove(make_title_menu)
+
+if __name__ == "__main__":
+    register()
